73fb521c06629c85ed7ffd14f1856d5aeb98614c
Skyler Lipthay <skyler.lipthay@gmail.com>
2015-02-15 17:28:27 -0800

    (feat) Use Option instead of Fork

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,17 @@
-/// A type that is used to determine whether the outer function should continue execution or end
-/// with a conclusion value.
-#[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug)]
-pub enum Fork<T> {
-    Conclude(T),
-    Proceed,
-}
-
-/// Aborts the outer function if given `Conclude` by returning its value, or continues the function
-/// given if `Proceed`. This is particularly useful for understandable extension injection site
+/// Aborts the outer function if given `Some` by returning its value, or continues the function
+/// if given `None`. This is particularly useful for understandable extension injection site
 /// specification.
 ///
 /// # Example
 ///
 /// ```ignore
-/// fn reject_if_wrong_password(password: &str) -> Fork<bool> {
+/// fn reject_if_wrong_password(password: &str) -> Option<bool> {
 ///     match password {
-///         "let_me_in" => Fork::Proceed,
+///         "let_me_in" => None,
 ///         _ => {
 ///             // Hide the responsibility of recording the failure from `test_password()`
 ///             record_failed_login_attempt();
-///             Fork::Conclude(false)
+///             Some(false)
 ///         },
 ///     }
 /// }
@@ -32,9 +24,7 @@ pub enum Fork<T> {
 #[macro_export]
 macro_rules! diverge {
     ($e:expr) => ({
-        use $crate::Fork::Conclude;
-
-        if let Conclude(e) = $e {
+        if let Some(e) = $e {
             return e;
         }
     })
@@ -42,19 +32,17 @@ macro_rules! diverge {
 
 #[cfg(test)]
 mod tests {
-    use super::Fork;
-
     #[test]
     fn test_conclude() {
-        assert_eq!(conclude_or_20(Fork::Conclude(10)), 10);
+        assert_eq!(conclude_or_20(Some(10)), 10);
     }
 
     #[test]
     fn test_proceed() {
-        assert_eq!(conclude_or_20(Fork::Proceed), 20);
+        assert_eq!(conclude_or_20(None), 20);
     }
 
-    fn conclude_or_20(fork: Fork<u32>) -> u32 {
+    fn conclude_or_20(fork: Option<u32>) -> u32 {
         diverge!(fork);
         20
     }
